f625e32693157df9308ca5c3efd74f9574ff95ca
Cedric Sarigumba <cediesarigumba@gmail.com>
2021-12-03 19:24:17 +0800

    get course impl

--- src/api/get-course.js
+++ src/api/get-course.js
@@ -1,30 +1,37 @@
-const AWS = require("aws-sdk");
-const dynamodb = new AWS.DynamoDB({
-  apiVersion: "2012-08-10"
-});
+const { GetItemCommand } = require('@aws-sdk/client-dynamodb');
+const { marshall, unmarshall } = require('@aws-sdk/util-dynamodb');
+const db = require('./database/db');
+const utils = require('./common/utils');
+const { HTTP_OK, HTTP_ERROR } = require('./common/http-status');
 
-exports.handler = (event, context, callback) => {
-  const params = {
-    Key: {
-      id: {
-        S: event.id
-      }
-    },
-    TableName: "courses"
-  };
-  dynamodb.getItem(params, (err, data) => {
-    if (err) {
-      console.log(err);
-      callback(err);
+exports.handler = async event => {
+  console.info(`Received event: ${JSON.stringify(event)}`);
+
+  try {
+    const courseId = event.pathParameters.id;
+
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE_NAME,
+      Key: marshall({ id: courseId }),
+    };
+
+    console.info(`Retrieving item in database. CourseId=${courseId}`);
+    let { Item: course } = await db.send(new GetItemCommand(params));
+
+    if (course) {
+      course = course ? unmarshall(course) : {};
+      console.info(`Successfully retrieved item. Course=${JSON.stringify(course)}`);
     } else {
-      callback(null, {
-        id: data.Item.id.S,
-        title: data.Item.title.S,
-        watchHref: data.Item.watchHref.S,
-        authorId: data.Item.authorId.S,
-        length: data.Item.length.S,
-        category: data.Item.category.S
-      });
+      console.info(`Course not exist. CourseId=${courseId}`);
     }
-  });
+
+    return utils.buildSuccessResponse({
+      data: course,
+      message: '',
+      statusCode: HTTP_OK,
+    });
+  } catch (error) {
+    console.error(`An error occurred during saving to database. ${error}`);
+    return utils.buildFailureResponse(error, HTTP_ERROR);
+  }
 };
