758e2311beb45c1bff8cfaf7fa1a3206e6e2f77f
Cedric Sarigumba <cediesarigumba@gmail.com>
2021-12-04 20:34:33 +0800

    get all authors implementation:

--- src/api/get-all-authors.js
+++ src/api/get-all-authors.js
@@ -1,21 +1,29 @@
-const AWS = require("aws-sdk");
-const dynamodb = new AWS.DynamoDB({
-  apiVersion: "2012-08-10"
-});
+const db = require('./database/db');
+const { ScanCommand } = require('@aws-sdk/client-dynamodb');
+const { unmarshall } = require('@aws-sdk/util-dynamodb');
+const utils = require('./common/utils');
+const { HTTP_OK, HTTP_ERROR } = require('./common/http-status');
 
-exports.handler = (event, context, callback) => {
-  const params = {
-    TableName: "authors"
-  };
-  dynamodb.scan(params, (err, data) => {
-    if (err) {
-      console.log(err);
-      callback(err);
-    } else {
-      const authors = data.Items.map(item => {
-        return { id: item.id.S, firstName: item.firstName.S, lastName: item.lastName.S };
-      });
-      callback(null, authors);
-    }
-  });
+exports.handler = async event => {
+  console.info(`Received event: ${JSON.stringify(event)}`);
+
+  try {
+    const params = { TableName: process.env.DYNAMODB_TABLE_NAME };
+
+    console.info(`Running scan in database.`);
+    const { Items } = await db.send(new ScanCommand(params));
+    // TODO: Add while loop to handle next item for the scan operation
+    // TODO: Add pagination here
+
+    const courses = Items.map(course => unmarshall(course));
+    console.debug(`Scanned items=${JSON.stringify(courses)}`);
+
+    return utils.buildSuccessResponse({
+      data: courses,
+      statusCode: HTTP_OK,
+    });
+  } catch (error) {
+    console.error(`An error occurred. ${error}`);
+    return utils.buildFailureResponse(error, HTTP_ERROR);
+  }
 };
