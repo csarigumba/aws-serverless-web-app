6d1f77010028ff332c2ab98d437fe74a9765fb13
Cedric Sarigumba <cediesarigumba@gmail.com>
2021-12-04 19:42:16 +0800

    refactor create course

--- src/api/create-course.js
+++ src/api/create-course.js
@@ -2,26 +2,24 @@ const { PutItemCommand } = require('@aws-sdk/client-dynamodb');
 const { marshall } = require('@aws-sdk/util-dynamodb');
 const db = require('./database/db');
 const utils = require('./common/utils');
-const { HTTP_OK, HTTP_ERROR } = require('./common/http-status');
+const { HTTP_CREATED, HTTP_ERROR } = require('./common/http-status');
 
 exports.handler = async event => {
   console.info(`Received event: ${JSON.stringify(event)}`);
 
   try {
-    const body = JSON.parse(event.body);
+    const course = JSON.parse(event.body);
     const params = {
       TableName: process.env.DYNAMODB_TABLE_NAME,
-      Item: marshall(body || {}),
+      Item: marshall(course || {}),
     };
 
-    console.info(`Saving item to database. Item=${JSON.stringify(body)}, Table=${process.env.DYNAMODB_TABLE_NAME}.`);
-    const result = await db.send(new PutItemCommand(params));
+    console.info(`Saving item to database. Item=${JSON.stringify(course)}, Table=${process.env.DYNAMODB_TABLE_NAME}.`);
+    await db.send(new PutItemCommand(params));
 
     console.info(`Successfully saved item.`);
     return utils.buildSuccessResponse({
-      data: result,
-      message: 'Successfully created course',
-      statusCode: HTTP_OK,
+      statusCode: HTTP_CREATED,
     });
   } catch (error) {
     console.error(`An error occurred during saving to database. ${error}`);
