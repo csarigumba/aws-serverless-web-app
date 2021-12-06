a21eb7bf378965e0c357aa855f588a92c7e5a565
Cedric Sarigumba <cediesarigumba@gmail.com>
2021-12-06 21:40:47 +0800

    updated all authors impl

--- src/api/get-all-authors.js
+++ src/api/get-all-authors.js
@@ -1,6 +1,4 @@
-const db = require('./database/db');
-const { ScanCommand } = require('@aws-sdk/client-dynamodb');
-const { unmarshall } = require('@aws-sdk/util-dynamodb');
+const { scanTable } = require('./helpers/dynamodb');
 const utils = require('./helpers/utils');
 const { HTTP_OK, HTTP_ERROR } = require('./common/http-status');
 
@@ -8,18 +6,11 @@ exports.handler = async event => {
   console.info(`Received event: ${JSON.stringify(event)}`);
 
   try {
-    const params = { TableName: process.env.DYNAMODB_TABLE_NAME };
-
-    console.info(`Running scan in database.`);
-    const { Items } = await db.send(new ScanCommand(params));
-    // TODO: Add while loop to handle next item for the scan operation
-    // TODO: Add pagination here
-
-    const courses = Items.map(course => unmarshall(course));
-    console.debug(`Scanned items=${JSON.stringify(courses)}`);
+    const authors = await scanTable(process.env.DYNAMODB_TABLE_NAME);
+    console.debug(`Scanned items=${JSON.stringify(authors)}`);
 
     return utils.buildSuccessResponse({
-      data: courses,
+      data: authors,
       statusCode: HTTP_OK,
     });
   } catch (error) {
